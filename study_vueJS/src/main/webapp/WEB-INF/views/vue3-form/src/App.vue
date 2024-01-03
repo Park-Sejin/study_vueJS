bd00bc2ecac52f2560b6a866e7233fed2b961b93
Park-Sejin <stsj84@gmail.com>
2024-01-03 14:34:34 +0900

    [2024/01/03] 1. [인프런] vue 3 시작하기  - 간단한 로그인 폼 생성  - 폼 이벤트 제어 및 서버로 데이터 전송 예제

---
+++ study_vueJS/src/main/webapp/WEB-INF/views/vue3-form/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form action="" @submit.prevent="submitForm">
+      <div>
+        <label for="userId">ID : </label>
+        <input type="text" id="userId" v-model="userId">
+      </div>
+      <div>
+        <label for="userPass">PW : </label>
+        <input type="text" id="userPass" v-model="userPass">
+      </div>
+      <button type="submit">로그인</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        userId : '',
+        userPass : ''
+      }
+    },
+    methods : {
+      submitForm() {
+        const data = {
+          userId : this.userId,
+          userPass : this.userPass
+        };
+
+        // fake url : https://jsonplaceholder.typicode.com/
+        axios.post('https://jsonplaceholder.typicode.com/users', data)
+          .then(response => {
+            console.log(response)
+          });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
