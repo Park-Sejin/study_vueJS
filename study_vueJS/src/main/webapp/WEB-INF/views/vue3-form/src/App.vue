1310e64841371bc6917dbe060e1f263ba1429850
Park-Sejin <stsj84@gmail.com>
2024-01-03 14:57:49 +0900

    [2024/01/03] 1. [인프런] vue 3 시작하기  - 옵션 API를 컴포지션 API로 변경

--- study_vueJS/src/main/webapp/WEB-INF/views/vue3-form/src/App.vue
+++ study_vueJS/src/main/webapp/WEB-INF/views/vue3-form/src/App.vue
@@ -16,28 +16,49 @@
 
 <script>
   import axios from 'axios'
+  import { ref } from 'vue';
 
   export default {
-    data() {
-      return {
-        userId : '',
-        userPass : ''
-      }
-    },
-    methods : {
-      submitForm() {
-        const data = {
-          userId : this.userId,
-          userPass : this.userPass
-        };
+    // 컴포지션 API
+    setup() {
+      // data - reactivity가 주입된 id와 pw를 ref라는 형태로 나타냄
+      var userId = ref('');
+      var userPass = ref('');
 
+      // methods - 함수 표현식으로 작성함
+      var submitForm = () => {
         // fake url : https://jsonplaceholder.typicode.com/
-        axios.post('https://jsonplaceholder.typicode.com/users', data)
-          .then(response => {
-            console.log(response)
-          });
+        axios.post('https://jsonplaceholder.typicode.com/users', {
+          // ref로 지정한 변수 안에 value가 실제 사용하는 값
+          userId : userId.value,
+          userPass : userPass.value
+        }).then(response => {
+          console.log(response);
+        });
       }
-    }
+
+      // setup에서 return된 값들은 전부 인스턴스 옵션과 <template> 내에서 사용 가능
+      return { userId, userPass, submitForm }
+    },
+
+    // 옵션 API
+    // data() {
+    //   return {
+    //     userId : '',
+    //     userPass : ''
+    //   }
+    // },
+    // methods : {
+    //   submitForm() {
+    //     // fake url : https://jsonplaceholder.typicode.com/
+    //     axios.post('https://jsonplaceholder.typicode.com/users', {
+    //       userId : this.userId,
+    //       userPass : this.userPass
+    //     }).then(response => {
+    //       console.log(response)
+    //     });
+    //   }
+    // }
   }
 </script>
 
